4abd2d8467fada1194d8846fdee2f9aa1c2bc3a6
anfal11 <anfal15-14303@diu.edu.bd>
2024-11-18 03:15:58 +0600

    log fixation

--- src/docker/docker.service.ts
+++ src/docker/docker.service.ts
@@ -1,31 +1,66 @@
+// // import { Injectable } from '@nestjs/common';
+
+// // @Injectable()
+// // export class DockerService {}
+
+
 // import { Injectable } from '@nestjs/common';
+// import Docker from 'dockerode';
 
 // @Injectable()
-// export class DockerService {}
+// export class DockerService {
+//   private docker: Docker;
 
+//   constructor() {
+//     this.docker = new Docker({ socketPath: '/var/run/docker.sock' });
+//   }
 
-import { Injectable } from '@nestjs/common';
-import Docker from 'dockerode';
+//   async listContainers() {
+//     return await this.docker.listContainers({ all: true });
+//   }
+
+//   async startContainer(containerId: string) {
+//     const container = this.docker.getContainer(containerId);
+//     return await container.start();
+//   }
+
+//   async stopContainer(containerId: string) {
+//     const container = this.docker.getContainer(containerId);
+//     return await container.stop();
+//   }
+// }
+import { Injectable, Logger } from '@nestjs/common';
+const Docker = require('dockerode');
 
 @Injectable()
 export class DockerService {
-  private docker: Docker;
+  private readonly logger = new Logger(DockerService.name);
+  private docker: any;
 
   constructor() {
     this.docker = new Docker({ socketPath: '/var/run/docker.sock' });
   }
 
   async listContainers() {
-    return await this.docker.listContainers({ all: true });
+    this.logger.log('Fetching container list...');
+    const containers = await this.docker.listContainers({ all: true });
+    this.logger.log(`Found containers: ${JSON.stringify(containers)}`);
+    return containers;
   }
 
   async startContainer(containerId: string) {
+    this.logger.log(`Starting container: ${containerId}`);
     const container = this.docker.getContainer(containerId);
-    return await container.start();
+    const result = await container.start();
+    this.logger.log(`Start result: ${JSON.stringify(result)}`);
+    return result;
   }
 
   async stopContainer(containerId: string) {
+    this.logger.log(`Stopping container: ${containerId}`);
     const container = this.docker.getContainer(containerId);
-    return await container.stop();
+    const result = await container.stop();
+    this.logger.log(`Stop result: ${JSON.stringify(result)}`);
+    return result;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+import { Logger, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('API');
+
+  app.use((req, res, next) => {
+    logger.log(`Incoming Request: ${req.method} ${req.url}`);
+    next();
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
