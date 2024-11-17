70d6cb8aa34ff88b32d1fb5e516fcb5b1fbe1126
anfal11 <anfal15-14303@diu.edu.bd>
2024-11-18 03:28:27 +0600

    package updated and added for main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,27 @@
 //   await app.listen(process.env.PORT ?? 3000);
 // }
 // bootstrap();
-import { Logger, NestFactory } from '@nestjs/core';
+// import { Logger, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const logger = new Logger('API');
+
+//   app.use((req, res, next) => {
+//     logger.log(`Incoming Request: ${req.method} ${req.url}`);
+//     next();
+//   });
+//   await app.listen(3000);
+// }
+// bootstrap();
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common'; // Correct import for older versions
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('API');
-
-  app.use((req, res, next) => {
-    logger.log(`Incoming Request: ${req.method} ${req.url}`);
-    next();
-  });
+  const logger = new Logger('Bootstrap'); // Example of using Logger
   await app.listen(3000);
+  logger.log('Application is running on http://localhost:3000');
 }
 bootstrap();
